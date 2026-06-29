2507430713a88f8d348d7215fb856455516156ba
Memo <fretn655@gmail.com>
2026-06-29 17:34:25 -0600

    se hace la primera parte del proyecto, donde se lee el archivo csv a utilizar para el modelo y se agregan unas partes para poder analizar distintos aspectos del df

--- Proyecto Luis Torres TP 11.py
+++ Proyecto Luis Torres TP 11.py
@@ -2,4 +2,14 @@
 
 import pandas as pd
 
-# ----------- Importaciones
+# ----------- Fin de las Importaciones
+
+# 1. Abre y examina el archivo de datos. Dirección al archivo
+df = pd.read_csv(r"C:\\Users\\sasor\\Desktop\\Tripleten\\Sprint 10\\Proyecto\\users_behavior.csv")
+
+"""esto nos permite revisar que se haya cargado correctamente"""
+print(df.head())
+
+"""aqui se verifica el tipo de datos de cada una"""
+print(df.info())
+print(df.dtypes)
