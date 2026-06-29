29db970dccdd6dfa840c4a3c780ad6498cf586e9
Memo <fretn655@gmail.com>
2026-06-29 17:47:18 -0600

    se segmentan los datos de forma 3:1:1. 60 train, 20 prueba, 20 validacion

--- Proyecto Luis Torres TP 11.py
+++ Proyecto Luis Torres TP 11.py
@@ -1,6 +1,7 @@
 # ----------- Importaciones
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # ----------- Fin de las Importaciones
 
@@ -13,3 +14,28 @@ print(df.head())
 """aqui se verifica el tipo de datos de cada una"""
 print(df.info())
 print(df.dtypes)
+
+# 2. Segmenta los datos fuente en un conjunto de entrenamiento, uno de validación y uno de prueba.
+
+"""dado que se reconoce que todas las columnas tienen el tipo de dato correcto se procede directo sin hacer alguna limpieza del df"""
+
+# dividir todo en 3 partes con proporción 3:1:1 - (60% entrenamiento, 20% validación, 20% prueba).
+
+features = df.drop(["is_ultra"], axis=1)
+target = df["is_ultra"]
+
+features_train, features_rest, target_train, target_rest = train_test_split(features, target, test_size=0.4, random_state=12345)
+
+features_valid, features_test, target_valid, target_test = train_test_split(
+    features_rest, target_rest, test_size=0.5, random_state=12345)
+
+"""Verificacion de que las proporciones de clase se mantuvieron"""
+
+print("Entrenamiento:", features_train.shape, target_train.shape)
+print("Validación", features_valid.shape, target_valid.shape)
+print("Prueba", features_test.shape, target_test.shape)
+
+print("Entrenamiento", target_train.mean())
+print("Validación", target_valid.mean())
+print("Prueba", target_test.mean())
+
